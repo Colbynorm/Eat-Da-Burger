9f90ee6e3abd755d3a75c65cdcb565c1de1aac2e
Colby Norman <colbynorm41@gmail.com>
2020-03-22 11:07:35 -0500

    added to schema.sql file

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers_db
+CREATE TABLE burgers
 (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(50) NOT NULL,
+    burgerName VARCHAR(50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY (id)
 );
